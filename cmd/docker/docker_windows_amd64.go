80589a2e587d818dcd785ff5b9544fe73be06d97
Petros Angelatos <petrosagg@gmail.com>
2017-07-25 16:46:51 -0700

    cmd/docker: export main package as a library
    Allows it to be used as part of a busybox-like binary
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- cmd/docker/docker_windows_amd64.go
+++ cmd/docker/docker_windows_amd64.go
@@ -3,6 +3,6 @@
 
 //go:generate goversioninfo -64=true -o=../../cli/winresources/resource.syso -icon=winresources/docker.ico -manifest=winresources/docker.exe.manifest ../../cli/winresources/versioninfo.json
 
-package main
+package docker
 
 import _ "github.com/docker/cli/cli/winresources"
