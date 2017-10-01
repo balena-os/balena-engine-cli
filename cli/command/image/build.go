ad016aefced10863a341b1471af886cf31783296
Petros Angelatos <petrosagg@gmail.com>
2017-10-01 09:00:22 -0700

    image/build: add option for specifying bind mounts
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- cli/command/image/build.go
+++ cli/command/image/build.go
@@ -68,6 +68,7 @@ type buildOptions struct {
 	stream         bool
 	platform       string
 	untrusted      bool
+	volumes        opts.ListOpts
 }
 
 // dockerfileFromStdin returns true when the user specified that the Dockerfile
@@ -90,6 +91,7 @@ func newBuildOptions() buildOptions {
 		ulimits:    opts.NewUlimitOpt(&ulimits),
 		labels:     opts.NewListOpts(opts.ValidateLabel),
 		extraHosts: opts.NewListOpts(opts.ValidateExtraHost),
+		volumes:    opts.NewListOpts(nil),
 	}
 }
 
@@ -152,6 +154,7 @@ func NewBuildCommand(dockerCli command.Cli) *cobra.Command {
 	flags.BoolVar(&options.squash, "squash", false, "Squash newly built layers into a single new layer")
 	flags.SetAnnotation("squash", "experimental", nil)
 	flags.SetAnnotation("squash", "version", []string{"1.25"})
+	flags.VarP(&options.volumes, "volume", "v", "Bind mount a volume")
 
 	flags.BoolVar(&options.stream, "stream", false, "Stream attaches to server to negotiate build context")
 	flags.MarkHidden("stream")
@@ -570,5 +573,6 @@ func imageBuildOptions(dockerCli command.Cli, options buildOptions) types.ImageB
 		ExtraHosts:     options.extraHosts.GetAll(),
 		Target:         options.target,
 		Platform:       options.platform,
+		Volumes:        options.volumes.GetAll(),
 	}
 }
